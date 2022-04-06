37b08fea13bb5565bf85f84783c793d69c16228e
Himanshu Raj <81401794+masterHimanshu@users.noreply.github.com>
2022-04-06 15:37:29 +0530

    make it better

--- main.py
+++ main.py
@@ -11,33 +11,9 @@ app = Flask(__name__)
 # model = pickle.load(open('model.pkl', 'rb'))
 
 
-# @app.route('/')
-# def home():
-#     return "hello world"
-
-
-def predict1(list2):
-    data = {"product name": ["bat", "ball", "iphone", "charger", "snacks"],
-            "product price": [12, 15, 99945, np.NaN, 80],
-            "tags": ["sports", "sports", "sports", "electronic", "food"], "rating": [4500, 4000, 600, 70, 5000]}
-    df = pd.DataFrame(data)
-    print(df)
-    # print("list2: " + list2)
-    df.set_index('product name', inplace=True)
-
-    list1 = []
-    # list2=["sports","electronic","food"]
-    for i in range(0, len(list2)):
-        print(len(list2))
-    for tgs in list2:
-        df23 = df[df['tags'] == tgs]
-        print(tgs)
-        df24 = df23.sort_values(by=['rating'], ascending=False)
-        df25 = df24[:2]
-        df26 = df25.head()
-        list1 += list(df26.index.values)
-    print(list1)
-    return list1
+@app.route('/')
+def home():
+    return "hello world"
 
 
 @app.route('/predict', methods=['POST'])
@@ -71,6 +47,30 @@ def predict():
     # Take the first value of prediction
     #  result1 = result
     return jsonify({'values': result1})
+
+
+def predict1(list2):
+    data = {"product name": ["bat", "ball", "iphone", "charger", "snacks"],
+            "product price": [12, 15, 99945, np.NaN, 80],
+            "tags": ["sports", "sports", "sports", "electronic", "food"], "rating": [4500, 4000, 600, 70, 5000]}
+    df = pd.DataFrame(data)
+    print(df)
+    # print("list2: " + list2)
+    df.set_index('product name', inplace=True)
+
+    list1 = []
+    # list2=["sports","electronic","food"]
+    for i in range(0, len(list2)):
+        print(len(list2))
+    for tgs in list2:
+        df23 = df[df['tags'] == tgs]
+        print(tgs)
+        df24 = df23.sort_values(by=['rating'], ascending=False)
+        df25 = df24[:2]
+        df26 = df25.head()
+        list1 += list(df26.index.values)
+    print(list1)
+    return list1
 
 
 if __name__ == '__main__':
